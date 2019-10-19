98a8d3646ee884fe9454b3479b98b2508c41429e
H4ck3rM4n <apavalac@student.42.fr>
2019-10-19 21:38:03 +0300

    This is sql script

--- sql/db.sql
+++ sql/db.sql
@@ -1,26 +1,13 @@
 CREATE TABLE biblioteca_db.Activity (
 	ActivityId int,
-	ActivityName varchar(255)
-);
-
-CREATE TABLE biblioteca_db.First (
-	KeyId int,
-	Tip varchar(255),
-	Filiala varchar(255),
-	Activ int,
-	NrParitici int
-);
-
-CREATE TABLE biblioteca_db.Second (
-	KeyId int,
-	Filiala varchar(255),
-	NrLivrate int,
-	NrServicii16 int,
-	NrTotalFormal int,
-	NrTotalNFormal int,
-	NrTotalIFormal int,
-	NrTotalUseri int,
-	NrTotalCopii int,
-	NrPartIFormal int,
-	NrPartICopii int
+	ActivityType ENUM ('Stiintifice', 'Culturale'),
+	ActivityFormat ENUM ('Conferinta', 'Simpozion', 'Lectie publica', 'Masa rotunda', 'Colocviu'),
+	ActivityDate DATE,
+	ActivityLocation varchar(255),
+	ActivityNrLocuri int,
+	ActivityNrLOcuriLibere int,
+	ActivityDescriere varchar(65000),
+	ActivityAudienta ENUM ('Copii', 'Tineri', 'Adulti', 'Seniori'),
+	ActivityTopic ENUM ('Teatru', 'Muzica', 'Dans'),
+	ActivityLimba ENUM ('Rusa', 'Romana', 'Engleza')
 );
