539f14cb8d17bd8036cd2b8f0383117d3f76d78c
rustingoff <nailunho@gmail.com>
2019-10-20 03:55:00 +0300

    update

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -50,10 +50,25 @@ $(document).ready(function() {
         dataType: "json",
         cache: false,
         timeout: 1000,
+        data: { get_param: 'value' },
         success: function(data) {
-        	console.log(data);
-        	$("#customers").html("<p>" + JSON.stringify(data )+ "</p>");
-        },
+
+        	let content = "<tr><th>Tip</th><th>Format</th><th>Data de desfasurare</th><th>Localul</th><th>Numarul total de locuri</th><th>Numarul de locuri disponibile</th><th>Descriere</th><th>Audienta</th><th>Topic</th><th>Limba</th></tr>"
+        	    $.each(data.rows, function(key, value){
+        		content +=	"" + "<tr><th>" + value.tip + 
+        				 	"</th><th>" + value.format +
+        				 	"</th><th>" + value.dataDesfasurare + 
+        				 	"</th><th>" + value.locatie + 
+        				 	"</th><th>" + value.nrTotalLocuri + 
+        				 	"</th><th>" + value.nrLocuriDisponibile + 
+        				 	"</th><th>" + value.descriere + 
+        				 	"</th><th>" + value.audienta + 
+        				 	"</th><th>" + value.topic + 
+        				 	"</th><th>" + value.limba + "</th></tr>"
+        	    
+        		$("#customers").html(content);
+        	});
+          },
         error: function(jqXHR, textStatus, errorThrown) {
             alert('error ' + textStatus + " " + errorThrown);
         }
